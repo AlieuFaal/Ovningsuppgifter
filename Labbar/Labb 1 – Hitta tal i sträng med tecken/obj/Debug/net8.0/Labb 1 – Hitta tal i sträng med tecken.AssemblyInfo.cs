e367e0ef3a2157ba666bcb24373e80e1d3bd23bf
Alieu Faal <alieufaal10@gmail.com>
2024-09-17 22:10:06 +0200

    Update (OOP Uppgifter klara)

--- Labbar/Labb 1 – Hitta tal i sträng med tecken/obj/Debug/net8.0/Labb 1 – Hitta tal i sträng med tecken.AssemblyInfo.cs
+++ Labbar/Labb 1 – Hitta tal i sträng med tecken/obj/Debug/net8.0/Labb 1 – Hitta tal i sträng med tecken.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Labb 1 – Hitta tal i sträng med tecken")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77deeba703971e3e9ba11b6dc647cf713e86f37f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ade603d4328e97862a20103662281f8ecf2c88e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Labb 1 – Hitta tal i sträng med tecken")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Labb 1 – Hitta tal i sträng med tecken")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- OOP/1. Skriva ut namn på personer/Program.cs
+++ OOP/1. Skriva ut namn på personer/Program.cs
@@ -1,37 +1,96 @@
-﻿using System.Runtime.InteropServices;
-using Microsoft.VisualBasic;
+﻿Human human1 = new Human() {firstName = "Alle", lastName = "Falle"};
 
-Person myPerson = new Person() {firstName = "Kalle", lastName = "Balle"};
+Human human2 = new Human() {firstName = "Alieu", lastName = "Faal", Mother = "Mariam Moore", Father = "Pa Faal"};
 
-Person otherPerson = new Person() {firstName = "Alle", lastName = "Balle"};
+Human human3 = new Human() {firstName = "Mariam", lastName = "Moore" };
 
-myPerson.GetFullName();
-otherPerson.GetFullName();
-Console.WriteLine(myPerson.GetFullNameReversed());
-System.Console.WriteLine(myPerson.GetFullName());
+Human human4 = new Human() {firstName = "Pa", lastName = "Faal" };
 
-class Person
+
+
+
+// Console.WriteLine(human1.GetFullName());
+// Console.WriteLine(human2.GetFullName());
+// Console.WriteLine(human1.GetFullNameReversed());
+// Console.WriteLine(human2.GetFullNameReversed());
+Console.WriteLine(human2.Mother);
+Console.WriteLine(human2.Father);
+Console.WriteLine(human2.GetFullName("Mr."));
+Console.WriteLine(human2.GetParents());
+System.Console.WriteLine(human2.GetSelfAndParents());
+human2.SetLength(1.94);
+Console.WriteLine(human2.GetLength());
+human2.SetWeight(90);
+Console.WriteLine(human2.GetWeight());
+Console.WriteLine(human2.GetBMI());
+
+
+Console.ReadKey();
+
+
+class Human
 {
     public string firstName;
 
     public string lastName;
 
-    public string GetFullName(string title = "Dr")
+    public string Mother;
+
+    public string Father;
+
+    private double length;
+
+    private double Weight;
+
+    public void SetLength(double length)
     {
-        return $"{title} {lastName}";
+        this.length = length;
+    }
+
+    public double GetLength()
+    {
+        return length;
+    }
+
+    public void SetWeight(double Weight)
+    {
+        this.Weight = Weight;
+    }
+
+    public double GetWeight()
+    {
+        return Weight;
+    }
+
+    public double GetBMI()
+    {
+        return Weight / Math.Pow(length, 2);
+    }
+
+    public string GetFullName(string title)
+    {
+        return $"{title} {firstName} {lastName}";
     }
 
     public string GetFullName()
-   {
-        return $" {firstName + " " + lastName}!";
-   }
+    {
+        return $"{firstName} {lastName}";
+    }
 
-   public string GetFullNameReversed()
-   {
-       char[] myArray = GetFullName().ToCharArray();
-       Array.Reverse(myArray);
-       return new string (myArray); 
-   }
+    public string GetFullNameReversed()
+    {
+       char[] nameArray = GetFullName().ToCharArray();
+       Array.Reverse(nameArray);
+       return new string (nameArray); 
+    }
 
+    public string GetParents()
+    {
+        return $"{Mother}, {Father}";
+    } 
 
+    public string GetSelfAndParents()
+    {
+        return $"{GetFullName()} - Mamma: {Mother} - Pappa: {Father}";
+    }
 }      
