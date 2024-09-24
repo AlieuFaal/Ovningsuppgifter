9b4274663e931a7045f069d6e644d214e04d744b
Alieu Faal <alieufaal10@gmail.com>
2024-09-24 23:42:19 +0200

    update

--- OOP/1. Skriva ut namn på personer/obj/Debug/net8.0/1. Skriva ut namn på personer.AssemblyInfo.cs
+++ OOP/1. Skriva ut namn på personer/obj/Debug/net8.0/1. Skriva ut namn på personer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1. Skriva ut namn på personer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8170ba23c5c67898deccfd39e5ba8b36dfcb4639")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e367e0ef3a2157ba666bcb24373e80e1d3bd23bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("1. Skriva ut namn på personer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1. Skriva ut namn på personer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
