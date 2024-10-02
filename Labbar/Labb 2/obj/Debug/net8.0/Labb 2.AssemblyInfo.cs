4c256d84c0cec7c048d66e32cf0b21eed197a066
Alieu Faal <alieufaal10@gmail.com>
2024-10-02 17:50:49 +0200

    update

--- Labbar/Labb 2/obj/Debug/net8.0/Labb 2.AssemblyInfo.cs
+++ Labbar/Labb 2/obj/Debug/net8.0/Labb 2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Labb 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72b8f9b18017f5f45a7540f3bd24df0382029742")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05da273826482f93ac72ba9b5d46b4b36ebdd093")]
 [assembly: System.Reflection.AssemblyProductAttribute("Labb 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Labb 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
